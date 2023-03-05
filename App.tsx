5ab194fea8c581ccd85fbc014933ad6388f0cb3f
itsaalexk <alexkononenko6@protonmail.com>
2023-03-05 22:10:31 +0100

    created store using Redux Toolkit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import {StatusBar} from 'expo-status-bar'
 import {StyleSheet, Text, View} from 'react-native'
+import {Provider} from 'react-redux'
+import {store} from './store/store'
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>Demo de React Native</Text>
-			<StatusBar style="auto" />
-		</View>
+		<Provider store={store}>
+			<View style={styles.container}>
+				<Text style={styles.text}>Demo de React Native</Text>
+				<StatusBar style="auto" />
+			</View>
+		</Provider>
 	)
 }
 
