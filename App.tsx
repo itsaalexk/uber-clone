945d6bb24188de76c837dde02079f6276dc5dc2e
itsaalexk <alexkononenko6@protonmail.com>
2023-03-06 08:49:00 +0100

    started project from scratch

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import {StatusBar} from 'expo-status-bar'
 import {StyleSheet, Text, View} from 'react-native'
-import {Provider} from 'react-redux'
-import {store} from './store/store'
 
 export default function App() {
 	return (
-		<Provider store={store}>
-			<View style={styles.container}>
-				<Text style={styles.text}>Demo</Text>
-				<StatusBar style="auto" />
-			</View>
-		</Provider>
+		<View style={styles.container}>
+			<Text style={styles.text}>Demo</Text>
+			<StatusBar style="auto" />
+		</View>
 	)
 }
 
